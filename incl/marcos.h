d5b0ec3157909621e3cdd148c17be02a7f1a6c35
gubnik <danger8217.nik@gmail.com>
2024-09-30 14:27:10 +0300

    Replaced a generic line in license header

--- incl/marcos.h
+++ incl/marcos.h
@@ -1,5 +1,5 @@
 /*
-        <one line to give the program's name and a brief idea of what it does.>
+        C-GC, a basic C garbage collector
         Copyright (C) 2024  Nikolay Gubankov (nikgub)
 
         This program is free software: you can redistribute it and/or modify
